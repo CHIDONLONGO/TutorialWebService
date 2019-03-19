59473c8acb1bf507768053c95bbda8fd0455bad8
CHIDONLONGO <jair.jcp@live.com.mx>
2019-03-19 00:04:07 -0600

    git clone

--- archivos/ejemploBDRemota/wsJSONConsultarListaImagenes.php
+++ archivos/ejemploBDRemota/wsJSONConsultarListaImagenes.php
@@ -8,7 +8,7 @@ $json=array();
 				
 		$conexion = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
 
-		$consulta="select * from usuario";
+		$consulta="select* from usuario";
 		$resultado=mysqli_query($conexion,$consulta);
 		
 		while($registro=mysqli_fetch_array($resultado)){
